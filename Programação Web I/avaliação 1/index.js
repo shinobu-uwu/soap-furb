6241892bba9270d15e7d2afd8c9471062c42c9ae
Matheus Filipe dos Santos Reinert <matheus.reinert@protonmail.com>
2022-05-01 20:30:49 -0300

    Coisas

--- Programação Web I/avaliação 1/index.js
+++ Programação Web I/avaliação 1/index.js
@@ -2,5 +2,5 @@ if (document.cookie.includes('username')) {
     let params = document.cookie.split(';');
     let username = params[0].split('=')[1];
     document.getElementById('user-login').innerHTML = `<p class="m-lg-0" style="padding-right: 12px;">${username}</p> 
-        <a href="login.html"><img width="50" height="50" src="avatar.webp" alt="Avatar do usuário" class="img-fluid img-thumbnail"></a>`
+        <a href="cadastro.html"><img width="50" height="50" src="avatar.webp" alt="Avatar do usuário" class="img-fluid img-thumbnail"></a>`
 }
