275220ac98ccc622dd168f6cd7255a9869ef0305
Matheus Filipe dos Santos Reinert <matheus.reinert@protonmail.com>
2022-08-26 19:27:49 -0300

    Fix

--- src/Server/FuncionarioServerImpl.java
+++ src/Server/FuncionarioServerImpl.java
@@ -6,26 +6,24 @@ import Ponto.Funcionario;
 import Ponto.FuncionarioControle;
 
 public class FuncionarioServerImpl implements FuncionarioServer {
-    private FuncionarioControle funcionarioControle = new FuncionarioControle();
 
     @Override
-    public void criar(Empresa empresa, String nomeFuncionario) throws ParametroInvalidoException {
-        funcionarioControle.CriarFuncionario(nomeFuncionario, empresa);
+    public void criar(String nomeFuncionario) throws ParametroInvalidoException {
+
     }
 
     @Override
     public Empresa ler(String nomeFuncionario) throws Exception {
-        funcionarioControle.Ler()
+        return null;
     }
 
     @Override
     public void atualizar(Funcionario funcionario, String nome) throws ParametroInvalidoException {
-        funcionarioControle
-    }
 
+    }
 
     @Override
     public void deletar(Funcionario funcionario) {
-        funcionarioControle
+
     }
 }
